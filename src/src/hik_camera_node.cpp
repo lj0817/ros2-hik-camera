bc07460f3f48caa79095d9796adc01845fa9d1b4
lj0817 <3348970664@qq.com>
2025-02-19 19:51:15 +0800

    增加修改分辨率和断线重连

--- src/src/hik_camera_node.cpp
+++ src/src/hik_camera_node.cpp
@@ -32,9 +32,7 @@ public:
     MV_CC_GetImageInfo(camera_handle_, &img_info_);
     image_msg_.data.reserve(img_info_.nHeightMax * img_info_.nWidthMax * 3);
 
-    convert_param_.nWidth = img_info_.nWidthValue;
-    convert_param_.nHeight = img_info_.nHeightValue;
-    convert_param_.enDstPixelType = PixelType_Gvsp_RGB8_Packed;
+    convert_param_.enDstPixelType = PixelType_Gvsp_RGB8_Packed; // Width and height will be set dynamically
 
     bool use_sensor_data_qos = this->declare_parameter("use_sensor_data_qos", true);
     auto qos = use_sensor_data_qos ? rmw_qos_profile_sensor_data : rmw_qos_profile_default;
@@ -56,8 +54,14 @@ public:
       image_msg_.encoding = "rgb8";
 
       while (rclcpp::ok()) {
-        nRet = MV_CC_GetImageBuffer(camera_handle_, &out_frame, 1000);
+        {
+          std::lock_guard<std::mutex> lock(camera_mutex_);
+          nRet = MV_CC_GetImageBuffer(camera_handle_, &out_frame, 1000);
+        }
+
         if (MV_OK == nRet) {
+          convert_param_.nWidth = out_frame.stFrameInfo.nWidth;
+          convert_param_.nHeight = out_frame.stFrameInfo.nHeight;
           convert_param_.pDstBuffer = image_msg_.data.data();
           convert_param_.nDstBufferSize = image_msg_.data.size();
           convert_param_.pSrcData = out_frame.pBufAddr;
@@ -79,10 +83,13 @@ public:
           fail_count_ = 0;
         } else {
           RCLCPP_WARN(this->get_logger(), "Get buffer failed!");
-          MV_CC_StopGrabbing(camera_handle_);
-          MV_CC_CloseDevice(camera_handle_);
-          MV_CC_DestroyHandle(camera_handle_);
-          camera_handle_ = nullptr;
+          {
+            std::lock_guard<std::mutex> lock(camera_mutex_);
+            MV_CC_StopGrabbing(camera_handle_);
+            MV_CC_CloseDevice(camera_handle_);
+            MV_CC_DestroyHandle(camera_handle_);
+            camera_handle_ = nullptr;
+          }
           fail_count_++;
         }
 
@@ -117,7 +124,9 @@ private:
     RCLCPP_INFO(this->get_logger(), "Attempting to reconnect...");
     MV_CC_DEVICE_INFO_LIST stDeviceList = {0};
     while (rclcpp::ok()) {
-      nRet = MV_CC_EnumDevices(MV_USB_DEVICE | MV_GIGE_DEVICE, &stDeviceList);
+      {
+        nRet = MV_CC_EnumDevices(MV_USB_DEVICE | MV_GIGE_DEVICE, &stDeviceList);
+      }
       if (nRet != MV_OK) {
         RCLCPP_ERROR(this->get_logger(), "Enum devices failed! nRet: [%x]", nRet);
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
@@ -125,7 +134,6 @@ private:
       }
 
       if (stDeviceList.nDeviceNum > 0) {
-
         nRet = MV_CC_CreateHandle(&camera_handle_, stDeviceList.pDeviceInfo[0]);
         if (nRet != MV_OK) {
           RCLCPP_ERROR(this->get_logger(), "Create handle failed! nRet: [%x]", nRet);
@@ -134,6 +142,10 @@ private:
 
         nRet = MV_CC_OpenDevice(camera_handle_);
         if (nRet == MV_OK) {
+          MV_CC_SetFloatValue(camera_handle_, "ExposureTime", exposuretime_);
+          MV_CC_SetFloatValue(camera_handle_, "Gain", gain_);
+          MV_CC_SetIntValueEx(camera_handle_, "Width", width_);
+          MV_CC_SetIntValueEx(camera_handle_, "Height", height_);
           MV_CC_StartGrabbing(camera_handle_);
           return true;
         } else {
@@ -156,13 +168,15 @@ private:
     MVCC_INTVALUE_EX i_h_value;
     param_desc.integer_range.resize(1);
     param_desc.integer_range[0].step = 1;
+
     // Exposure time
     param_desc.description = "Exposure time in microseconds";
     MV_CC_GetFloatValue(camera_handle_, "ExposureTime", &f_value);
     param_desc.integer_range[0].from_value = f_value.fMin;
     param_desc.integer_range[0].to_value = f_value.fMax;
-    double exposure_time = this->declare_parameter("exposure_time", 5000, param_desc);
+    double exposure_time = this->declare_parameter("exposure_time",f_value.fCurValue, param_desc);
     MV_CC_SetFloatValue(camera_handle_, "ExposureTime", exposure_time);
+    exposuretime_ = exposure_time;
     RCLCPP_INFO(this->get_logger(), "Exposure time: %f", exposure_time);
 
     // Gain
@@ -171,6 +185,7 @@ private:
     param_desc.integer_range[0].from_value = f_value.fMin;
     param_desc.integer_range[0].to_value = f_value.fMax;
     double gain = this->declare_parameter("gain", f_value.fCurValue, param_desc);
+    gain_ = gain;
     MV_CC_SetFloatValue(camera_handle_, "Gain", gain);
     RCLCPP_INFO(this->get_logger(), "Gain: %f", gain);
 
@@ -180,6 +195,7 @@ private:
     param_desc.integer_range[0].from_value = i_w_value.nMin;
     param_desc.integer_range[0].to_value = i_w_value.nMax;
     auto width = this->declare_parameter("width", i_w_value.nCurValue, param_desc);
+    width_ = width;
     MV_CC_SetIntValueEx(camera_handle_, "Width", width);
     RCLCPP_INFO(this->get_logger(), "Width: %ld", width);
 
@@ -189,10 +205,10 @@ private:
     param_desc.integer_range[0].from_value = i_h_value.nMin;
     param_desc.integer_range[0].to_value = i_h_value.nMax;
     auto height = this->declare_parameter("height", i_h_value.nCurValue, param_desc);
+    height_ = height;
     MV_CC_SetIntValueEx(camera_handle_, "Height", height);
     RCLCPP_INFO(this->get_logger(), "Height: %ld", height);
   }
-
 
   rcl_interfaces::msg::SetParametersResult parametersCallback(
     const std::vector<rclcpp::Parameter> & parameters)
@@ -201,37 +217,52 @@ private:
     result.successful = true;
     for (const auto & param : parameters) {
       if (param.get_name() == "exposure_time") {
-        int status = MV_CC_SetFloatValue(camera_handle_, "ExposureTime", param.as_int());
+        std::lock_guard<std::mutex> lock(camera_mutex_);
+        exposuretime_ = param.as_double();
+        int status = MV_CC_SetFloatValue(camera_handle_, "ExposureTime", exposuretime_);
+        RCLCPP_INFO(this->get_logger(), "Exposure time: %f", exposuretime_);
         if (MV_OK != status) {
           result.successful = false;
           result.reason = "Failed to set exposure time, status = " + std::to_string(status);
         }
       } else if (param.get_name() == "gain") {
-        int status = MV_CC_SetFloatValue(camera_handle_, "Gain", param.as_double());
+        std::lock_guard<std::mutex> lock(camera_mutex_);
+        gain_ = param.as_double();
+        RCLCPP_INFO(this->get_logger(), "Gain: %f", gain_);
+        int status = MV_CC_SetFloatValue(camera_handle_, "Gain", gain_);
         if (MV_OK != status) {
           result.successful = false;
           result.reason = "Failed to set gain, status = " + std::to_string(status);
         }
-      }else if (param.get_name() == "width") {
-        int status = MV_CC_SetIntValueEx(camera_handle_, "Width", param.as_int());
+      } else if (param.get_name() == "width") {
+        std::lock_guard<std::mutex> lock(camera_mutex_);
+        width_ = param.as_int();
+        RCLCPP_INFO(this->get_logger(), "Width: %d", width_);
+        MV_CC_StopGrabbing(camera_handle_);
+        int status = MV_CC_SetIntValueEx(camera_handle_, "Width", width_);
+        MV_CC_StartGrabbing(camera_handle_);
         if (MV_OK != status) {
           result.successful = false;
           result.reason = "Failed to set width, status = " + std::to_string(status);
         }
-      }else if (param.get_name() == "height") {
-        int status = MV_CC_SetIntValueEx(camera_handle_, "Height", param.as_int());
+      } else if (param.get_name() == "height") {
+        std::lock_guard<std::mutex> lock(camera_mutex_);
+        height_ = param.as_int();
+        RCLCPP_INFO(this->get_logger(), "Height: %d", height_);
+        MV_CC_StopGrabbing(camera_handle_);
+        int status = MV_CC_SetIntValueEx(camera_handle_, "Height", height_);
+        MV_CC_StartGrabbing(camera_handle_);
         if (MV_OK != status) {
           result.successful = false;
           result.reason = "Failed to set height, status = " + std::to_string(status);
         }
-      }else {
+      } else {
         result.successful = false;
         result.reason = "Unknown parameter: " + param.get_name();
       }
     }
     return result;
   }
-
 
   sensor_msgs::msg::Image image_msg_;
   image_transport::CameraPublisher camera_pub_;
@@ -244,6 +275,13 @@ private:
   std::thread capture_thread_;
   OnSetParametersCallbackHandle::SharedPtr params_callback_handle_;
   sensor_msgs::msg::CameraInfo camera_info_msg_;
+  std::mutex camera_mutex_;
+
+  float exposuretime_ = 0.0;
+  double gain_ = 0;
+  int width_ = 0;
+  int height_ = 0;
+
 };
 
 }  // namespace hik_camera
